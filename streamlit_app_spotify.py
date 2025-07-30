39733ad8717d21b9822803c75f0f7ce9fcd8124b
dmavunga <mavunga03@gmail.com>
2025-07-29 21:04:52 -0400

    Add files via upload

--- streamlit_app_spotify.py
+++ streamlit_app_spotify.py
@@ -1,25 +1,13 @@
 # streamlit_app_spotify
-import streamlit as st
-from st_supabase_connection import SupabaseConnection
 
-        # Initialize connection
-conn = st.connection(
-            name="supabase",
-            type=SupabaseConnection,
-            url=st.secrets["SUPABASE_URL"],
-            key=st.secrets["SUPABASE_KEY"]
-        )
+import streamlit as st 
+from supabase import create_client
 
-        # Example: Perform a query
-response = conn.query("*", table="artist").execute()
-
-        # Display results
-if response and response.data:
-            st.write("Data from Supabase:")
-            st.dataframe(response.data)
-else:
-            st.write("No data retrieved or query failed.")
-
-
-
-
+# Load credentials from secrets
+url = st.secrets["SUPABASE_URL"]
+key = st.secrets["SUPABASE_KEY"]
+# Create client
+supabase = create_client(url, key)
+# Example: fetch all rows from "users" table
+data = supabase.table("artist").select("*").execute()
+st.write(data.data)
